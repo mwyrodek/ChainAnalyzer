9735cf784a4e7d743b0de1311ef282c937fb6540
mwyrodek <wyrodek.maciej@gmail.com>
2019-11-17 19:59:20 +0100

    Changed Error code and Category

--- src/ChainAnalyzer/Analyzer1/Analyzer1/ChainLenghtAnalyzer.cs
+++ src/ChainAnalyzer/Analyzer1/Analyzer1/ChainLenghtAnalyzer.cs
@@ -15,14 +15,14 @@ namespace ChainAnalyzer
     public class ChainLenghtAnalyzer : DiagnosticAnalyzer
     {
         public const int MaxChainLength = 5;
-        public const string DiagnosticId = "ChainAnalyzer";
+        public const string DiagnosticId = "CHAIN001";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Usage";
+        private const string Category = "Readablity";
 
         private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
